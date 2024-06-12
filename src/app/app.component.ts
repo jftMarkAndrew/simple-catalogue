d8caf5a1044d7065278f19b6af1cd5cba0317b95
Mark Andrew <jftmain@gmail.com>
2024-06-13 02:21:29 +0300

    cdk scrollable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
 import { EditorComponent } from './components/editor/editor.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, MenuComponent, EditorComponent],
+  imports: [RouterOutlet, MenuComponent, EditorComponent, ScrollingModule],
 })
 export class AppComponent {
   title = 'simple-catalogue';
